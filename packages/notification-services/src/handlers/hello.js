f3307d547aef352688661602391dd7743444a3f3
Jorge Cuevas <jcq012@gmail.com>
2020-08-20 01:04:54 -0400

    queue message complete

--- packages/notification-services/src/handlers/hello.js
+++
@@ -1,8 +0,0 @@
-async function hello(event, context) {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ message: "hello" }),
-  }
-}
-
-export const handler = hello
---
+++ packages/notification-services/src/handlers/sendEmail.js
@@ -0,0 +1,36 @@
+import AWS from "aws-sdk"
+
+const ses = new AWS.SES({ region: "us-west-2" })
+async function sendEmail(event, context) {
+  const record = event.Records[0]
+  console.log("Procesing record:: ", record)
+
+  const email = JSON.parse(record.body)
+  const { subject, body, recipient } = email
+
+  const params = {
+    Source: "jcq012@gmail.com",
+    Destination: {
+      ToAddresses: [recipient],
+    },
+    Message: {
+      Body: {
+        Text: {
+          Data: body,
+        },
+      },
+      Subject: {
+        Data: subject,
+      },
+    },
+  }
+
+  try {
+    const result = await ses.sendEmail(params).promise()
+    console.log(result)
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+export const handler = sendEmail
